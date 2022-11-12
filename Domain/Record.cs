fa25d8b024aa60eaa34bc397acf60e71f9dea9e8
Shaun <shauneganza@gmail.com>
2022-11-12 19:17:04 +0200

    Record

---
+++ Domain/Record.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// Represents the number of tasks completed in a given cycle
+    /// </summary>
+    public class Record
+    {
+        /// <summary>
+        /// The number of tasks completed
+        /// </summary>
+        private readonly uint _completedTasks;
+
+        /// <summary>
+        /// Create an instance of Record, which represents the number of tasks completed in a given cycle
+        /// </summary>
+        /// <param name="completedTasks">The number of tasks completed in the given cycle</param>
+        public Record(uint completedTasks)
+        {
+            _completedTasks = completedTasks;
+        }
+
+        /// <summary>
+        /// Get the number of tasks completed
+        /// </summary>
+        /// <returns>The number of completed tasks</returns>
+        public uint GetCompletedTasks()
+        {
+            return _completedTasks;
+        }
+    }
+}
---
+++ DomainUnitTests/RecordTest.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using NUnit.Framework;
+
+namespace DomainUnitTests
+{
+    public class RecordTest
+    {
+        [Test]
+        public void TestZeroCompletedTasks()
+        {
+            var _ = new Record(0);
+        }
+
+        [Test]
+        public void TestValidNumberOfCompletedTasks()
+        {
+            var _ = new Record(10);
+        }
+    }
+}
