99e4d1508a5a974ba0744a60027c1badc955f6ef
Shaun <shauneganza@gmail.com>
2022-11-12 19:02:19 +0200

    Specification

---
+++ Domain/Simulation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Domain
+{
+    public interface IBuilder
+    {
+        void Iterations(int iterations);
+    }
+
+    public class SimulationBuilder : IBuilder
+    {
+        private Simulation _simulation = new Simulation();
+
+        public SimulationBuilder()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            _simulation = new Simulation();
+        }
+
+        public void Iterations(int iterations)
+        {
+            _simulation.IterationSpecification = new IterationSpecification(iterations);
+        }
+
+        public Simulation GetSimulation()
+        {
+            var result = this._simulation;
+            Reset();
+            return result;
+        }
+    }
+
+    public class Simulation
+    {
+        internal IterationSpecification IterationSpecification;
+    }
+}
---
+++ DomainUnitTests/IterationSpecificationTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Domain;
+using NUnit.Framework;
+
+namespace DomainUnitTests
+{
+    public class IterationSpecificationTest
+    {
+        [Test]
+        public void TestValidIterations()
+        {
+            var _ = new IterationSpecification(1);
+        }
+
+        [Test]
+        public void TestZeroIterations()
+        {
+            try
+            {
+                var _ = new IterationSpecification(0);
+                Assert.Fail("Expected an Exception to be thrown");
+            }
+            catch (ArgumentException _)
+            {
+                Assert.Pass();
+            }
+            catch (Exception _)
+            {
+                Assert.Fail("Expected an ArgumentException to be thrown");
+            }
+        }
+    }
+}
