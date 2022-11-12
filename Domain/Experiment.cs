2f30874293457d9f18da54d90a352df6fc38d822
Shaun <shauneganza@gmail.com>
2022-11-12 19:05:34 +0200

    Renamed to experiment

---
+++ Domain/Experiment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Domain
+{
+    public interface IBuilder
+    {
+        void Iterations(int iterations);
+    }
+
+    public class ExperimentBuilder : IBuilder
+    {
+        private Experiment _experiment = new Experiment();
+
+        public ExperimentBuilder()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            _experiment = new Experiment();
+        }
+
+        public void Iterations(int iterations)
+        {
+            _experiment.IterationSpecification = new IterationSpecification(iterations);
+        }
+
+        public Experiment GetSimulation()
+        {
+            var result = this._experiment;
+            Reset();
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// An experiment can be used to execute an arbitrary number of simulations, providing confidence intervals for burn
+    /// down.
+    /// </summary>
+    public class Experiment
+    {
+        internal IterationSpecification IterationSpecification;
+    }
+}
